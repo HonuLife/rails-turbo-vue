62c7d62ec782c0bca9231f8ceadfce245f4aaafc
Joe Krump <krump.joseph@gmail.com>
2023-07-01 21:51:04 -0400

    Handle routes with dynamic ids in them

--- app/frontend/helpers/routes.ts
+++ app/frontend/helpers/routes.ts
@@ -8,4 +8,13 @@ const routes = {
   "/pandas": [["#pandas-view", PandasApp]],
 };
 
-export const getVueComponents = (url: string) => routes[url];
+export const getVueComponents = (url: string) => {
+  const pattern = /^\/((?:[^\/]+\/)*[^\/]+)\/\d+(\/\w+)?$/;
+  const result = url.match(pattern);
+
+  if (result !== null) {
+    url = result[2] ? `/${result[1]}/:id${result[2]}` : `/${result[1]}/:id`;
+  }
+
+  return routes[url];
+};
