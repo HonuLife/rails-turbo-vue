a4aa50d1f74d70647bc547ed308a9ad89f9a919c
Joe Krump <krump.joseph@gmail.com>
2023-07-12 11:18:26 -0400

    Test homepage in dark mode

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 RSpec.describe 'home page', type: :system do
   it 'renders the home page with the expected content' do
-    page.goto('/')
+    page.goto('/', waitUntil: "domcontentloaded")
 
     page.screenshot(path: actual_screenshot_path)
 
     expect(actual_screenshot_path)
       .to match_screenshot(
         expected_screenshot_path(suffix: nil),
+        max_threshold_pct: 2.4
+      )
+
+    # Switch to customer prefers dark color scheme (aka dark mode)
+    page.emulate_media(colorScheme: 'dark')
+
+    # Test that the page looks correct in dark mode
+    page.screenshot(path: actual_screenshot_path(suffix: 'dark'))
+    expect(actual_screenshot_path(suffix: 'dark'))
+      .to match_screenshot(
+        expected_screenshot_path(suffix: 'dark'),
         max_threshold_pct: 2.4
       )
   end
