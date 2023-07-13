171fd379fa3e35409ccb46c769cc7916d9e2e114
Joe Krump <krump.joseph@gmail.com>
2023-07-12 20:32:59 -0400

    Continue setting up channel

--- app/channels/import_users_progress_channel.rb
+++ app/channels/import_users_progress_channel.rb
@@ -1,10 +1,15 @@
 class ImportUsersProgressChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    # stream_from "import_user_#{param[:user_id]}_progress_channel"
     stream_from 'import_users_progress_channel'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def get_progress
+    ActionCable.server.broadcast 'import_users_progress_channel', { progress: 0 }
   end
 end
