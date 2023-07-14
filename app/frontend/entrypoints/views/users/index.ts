7e922ac42302a0e5f7aeaec30a48337ef072a724
Joe Krump <krump.joseph@gmail.com>
2023-07-13 22:27:30 -0400

    Fix issues with tests

--- app/frontend/entrypoints/views/users/index.ts
+++
@@ -1,4 +0,0 @@
-import { mountComponentOnTurboLoad } from "@/helpers/mount-component-on-turbo-load";
-import App from "@/entrypoints/views/users/index/App.vue";
-
-mountComponentOnTurboLoad(App, "#root-view");
--- spec/channels/import_users_progress_channel_channel_spec.rb
+++ spec/channels/import_users_progress_channel_channel_spec.rb
@@ -1,5 +1,5 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe ImportUsersProgressChannelChannel, type: :channel do
+RSpec.describe ImportUsersProgressChannel, type: :channel do
   pending "add some examples to (or delete) #{__FILE__}"
 end
