aec8c9e01414b0f22ad9b458cdbef5eafd2c3975
Joe Krump <krump.joseph@gmail.com>
2023-07-15 22:38:27 -0400

    Allow for component dependencies to be declared and dynamically loaded

--- app/frontend/entrypoints/views/root/mount-apps.ts
+++ app/frontend/entrypoints/views/root/mount-apps.ts
@@ -1,6 +1,16 @@
 import RootApp from "@/entrypoints/views/root/RootApp.vue";
 import { mountComponent } from "@/helpers/mount-component";
 
-const mountRootApp = () => mountComponent("#root-view", RootApp);
+const mountRootApp = () => {
+  const componentDependencies = {
+    WelcomeItem: () => import("@/components/WelcomeItem.vue"),
+    IconDocumentation: () => import("@/components/icons/IconDocumentation.vue"),
+    IconTooling: () => import("@/components/icons/IconTooling.vue"),
+    IconEcosystem: () => import("@/components/icons/IconEcosystem.vue"),
+    IconCommunity: () => import("@/components/icons/IconCommunity.vue"),
+  };
+
+  return mountComponent("#root-view", RootApp, componentDependencies);
+};
 
 export { mountRootApp };
--- app/frontend/helpers/mount-component.ts
+++ app/frontend/helpers/mount-component.ts
@@ -1,13 +1,21 @@
-import type { Component } from "vue";
-import { createApp, type App } from "vue";
+import type { AsyncComponentLoader, Component } from "vue";
+import { createApp, type App, defineAsyncComponent } from "vue";
 
-export function mountComponent(querySelector: string, component: Component) {
+export function mountComponent(
+  querySelector: string,
+  component: Component,
+  componentDependencies?: Record<string, AsyncComponentLoader<Component>>
+) {
   const rootContainer = document.querySelector(querySelector) as HTMLElement;
   let app: App | undefined;
 
   if (rootContainer !== null) {
     const props = rootContainer.dataset.props;
     app = createApp(component, props ? JSON.parse(props) : undefined);
+
+    if (componentDependencies !== undefined) {
+      globallyRegisterComponentsOnApp(app, componentDependencies);
+    }
 
     app.mount(rootContainer);
     localStorage.removeItem("dynamic import failed count");
@@ -16,4 +24,13 @@ export function mountComponent(querySelector: string, component: Component) {
   }
 
   return app;
+}
+
+function globallyRegisterComponentsOnApp(
+  app: App,
+  components: Record<string, AsyncComponentLoader<Component>>
+) {
+  for (const [name, asyncComponentLoader] of Object.entries(components)) {
+    app.component(name, defineAsyncComponent(asyncComponentLoader));
+  }
 }
