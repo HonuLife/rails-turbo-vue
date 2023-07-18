577683569bbfdf2af01554b1cdce1c08ef5d1312
Joe Krump <krump.joseph@gmail.com>
2023-07-18 10:33:28 -0400

    Dynamically import and mount the Zap component on Pandas index view

--- app/frontend/entrypoints/views/pandas/index/mount-apps.ts
+++ app/frontend/entrypoints/views/pandas/index/mount-apps.ts
@@ -1,8 +1,12 @@
-import PandaInfo from "@/entrypoints/views/pandas/index/PandaInfo.vue";
-import Zap from "@/entrypoints/views/pandas/index/Zap.vue";
+import PandasInfo from "@/entrypoints/views/pandas/index/PandaInfo.vue";
 import { mountComponent } from "@/helpers/mount-component";
 
-const mountPandasInfo = () => mountComponent("#pandas-view", PandaInfo);
-const mountZap = () => mountComponent("#lazy-load", Zap);
+const mountPandasInfo = () => mountComponent("#pandas-view", PandasInfo);
+const mountZap = () =>
+  mountComponent(
+    "#lazy-load",
+    async () =>
+      (await import("@/entrypoints/views/pandas/index/Zap.vue")).default
+  );
 
 export { mountPandasInfo, mountZap };
